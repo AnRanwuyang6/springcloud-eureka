6e8a0c039c03bea81903050b1749645e2c9d3e7a
lizk <zong65432@163.com>
2019-12-19 09:55:51 +0800

    初始化代码

---
+++ src/test/java/com/lizk/eureka/cdeureka/CdEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lizk.eureka.cdeureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CdEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
